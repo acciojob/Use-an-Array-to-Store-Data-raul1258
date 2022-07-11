bc0c0a7aecb2978bca109da33b05b29c0e7a794b
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-07-11 21:26:00 +0530

    completed

--- solution.js
+++ solution.js
@@ -2,6 +2,7 @@
 let yourArray;
 // assign your array the correct value in the next line
 // your code here!
+yourArray = [5,"three",true,false,null,undefined];
 // code end
 
 module.exports = yourArray
